9b688f323d9199937ba3b09b8e7cc3ace4e398dd
thucvijul07 <108745183+thucvijul07@users.noreply.github.com>
2024-08-07 10:09:30 +0700

    Task #78910:[Role Guest. Role Customer] Thiết kế layout

--- src/main/java/cinemas/controllers/HomeController.java
+++ src/main/java/cinemas/controllers/HomeController.java
@@ -15,12 +15,12 @@ public class HomeController {
     @GetMapping("/")
     public String home(Model model) {
         var user = new User();
-        user.setName("Sun Asterisk");
-        user.setAddress("Japan");
+        user.setFullName("Sun Asterisk");
+        user.setEmail("Japan@b.com");
         var savedUser = userService.save(user);
         model.addAttribute("user", savedUser);
         userService.delete(savedUser);
-        return "user-info";
+        return "user/home-page";
     }
 
 }
