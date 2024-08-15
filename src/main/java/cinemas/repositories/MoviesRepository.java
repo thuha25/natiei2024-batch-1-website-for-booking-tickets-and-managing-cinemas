df6a4c2b05e521390c3f8352c2471e07408e7031
thucvijul07 <108745183+thucvijul07@users.noreply.github.com>
2024-08-15 23:51:51 +0700

    Task.79864 Task.78914

--- src/main/java/cinemas/repositories/MoviesRepository.java
+++ src/main/java/cinemas/repositories/MoviesRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 public interface MoviesRepository extends BaseRepository<Movie, Integer> {
     List<Movie> getMoviesByTitleAndStatus(String keyword, Pageable pageable, MovieStatus status);
     Integer countMoviesByTitleAndStatus(String keyword, MovieStatus status);
+    List<Movie> getMoviesByStatus(MovieStatus status);
 }
